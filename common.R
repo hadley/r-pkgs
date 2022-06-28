4edaaffeaba5df8c4d7c1a874843423c20c85ae7
Hadley Wickham <h.wickham@gmail.com>
2022-06-27 19:09:02 -0500

    More massive slicing and dicing

--- common.R
+++ common.R
@@ -4,8 +4,8 @@
 
 knitr::opts_chunk$set(
   comment = "#>",
-  collapse = TRUE
-  # cache = TRUE,
+  collapse = TRUE,
+  cache = TRUE
   # fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
   # dpi = 300,
   # out.width = "70%",
