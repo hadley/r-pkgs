10da5b969d44a323030c092f69bb39199605fee4
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-11-23 19:09:06 -0800

    Code useful for rationalizing labels

---
+++ notes/scrutinize-section-labels-in-one-chapter.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(parsermd)
+
+file <- "vignettes.Rmd"
+(rmd <- parse_rmd(file) |> 
+    rmd_select(has_type("rmd_heading")))
+# parsermd doesn't know about callouts, so they erroneously appear as sec_h2
+(dat <- as_tibble(rmd) |> 
+    select(matches("sec_h[23]")) |> 
+    filter(!str_detect(sec_h2, "Submitting to CRAN")) |> 
+    separate_wider_regex(
+      everything(),
+      c("text" = "[^\\{]*", " [{]", label = ".*", "[}]"),
+      names_sep = "_", too_few = "align_start"
+    ))
