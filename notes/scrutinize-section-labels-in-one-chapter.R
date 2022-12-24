bf00c8c862d4ecf551173ecb59adcbae21908860
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-12-23 20:32:06 -0800

    Add the Git/GitHub section
    Closes #911

--- notes/scrutinize-section-labels-in-one-chapter.R
+++ notes/scrutinize-section-labels-in-one-chapter.R
@@ -1,15 +1,19 @@
 library(tidyverse)
 library(parsermd)
 
-file <- "dependencies-in-practice.Rmd"
+file <- "website.Rmd"
 (rmd <- parse_rmd(file) |> 
     rmd_select(has_type("rmd_heading")))
+
 # parsermd doesn't know about callouts, so they erroneously appear as sec_h2
-(dat <- as_tibble(rmd) |> 
-    select(matches("sec_h[23]")) |> 
-    filter(!str_detect(sec_h2, "Submitting to CRAN")) |> 
-    separate_wider_regex(
-      everything(),
-      c("text" = "[^\\{]*", " [{]", label = ".*", "[}]"),
-      names_sep = "_", too_few = "align_start"
-    ))
+dat <- as_tibble(rmd) |> 
+  select(matches("sec_h[23]")) |> 
+  filter(!str_detect(sec_h2, "Submitting to CRAN")) |> 
+  separate_wider_regex(
+    everything(),
+    c("text" = "[^\\{]*", " [{]", label = ".*", "[}]"),
+    names_sep = "_", too_few = "align_start"
+  )
+dat |> 
+  select(ends_with("label")) |> 
+  print(n = Inf)
